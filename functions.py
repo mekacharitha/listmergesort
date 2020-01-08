ea9f0190a5d37457f03c9491760eba72199a355c
mekacharitha <charitha.silver@gmail.com>
2020-01-08 12:11:24 +0000

    Initial commit

---
+++ functions.py
@@ -0,0 +1,4 @@
+def mergesort(lis1,lis2):
+  lis = lis1+lis2
+  lis.sort()
+  return(lis)
---
+++ main.py
@@ -0,0 +1,4 @@
+import functions
+lis1=list(map(int,input().split()))
+lis2=list(map(int,input().split()))
+print(functions.mergesort(lis1,lis2))
